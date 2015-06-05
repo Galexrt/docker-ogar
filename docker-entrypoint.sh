f0455f2a138bc8543fa360319ff90436bc9cb60a
Alexander T. <Galexrt@users.noreply.github.com>
2015-06-05 09:43:18 +0200

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -z "$SERVER_PORT" ]; then
+    sed -i 's/serverPort = .*$/serverPort = '"$SERVER_PORT"'/g' "$DATA_PATH/gameserver.ini"
+fi
+if [ ! -z "$SERVER_GAMEMODE" ]; then
+    if [[ "$SERVER_GAMEMODE" == "0" && "$SERVER_GAMEMODE" == "1" ]]; then
+        echo "SERVER_GAMEMODE is invalid, defaulting to 0"
+        SERVER_GAMEMODE="0"
+    fi
+    sed -i 's/serverGamemode = .*$/serverGamemode = '"$SERVER_GAMEMODE"'/g' "$DATA_PATH/gameserver.ini"
+fi
+if [ ! -z "$SERVER_BOTS" ]; then
+    sed -i 's/serverPort = .*$/serverPort = '"$SERVER_BOTS"'/g' "$DATA_PATH/gameserver.ini"
+fi
+if [ ! -z "$SERVER_BOTS" ]; then
+    sed -i 's/serverPort = .*$/serverPort = '"$SERVER_BOTS"'/g' "$DATA_PATH/gameserver.ini"
+fi
+
+node Ogar
